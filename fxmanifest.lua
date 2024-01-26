cc87a90dabf289efad3e361a6e98272293a7da20
FD Tracka <157775231+Fdtracka@users.noreply.github.com>
2024-01-26 11:53:43 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- [[[https://discord.gg/fddev]]]
+-- [[[https://fd-development.tebex.io/]]]
+
+
+fx_version 'bodacious'
+game 'gta5'
+
+author 'FD Development'
+version '1.0.0'
+
+shared_script {
+    'config.lua'
+}
+
+client_script 'client.lua'
